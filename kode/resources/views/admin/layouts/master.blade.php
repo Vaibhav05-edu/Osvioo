50923856266f6b2ccaaa1f7136c30836307f76c4
Vaibhav <vaibhav.contact.in@gmail.com>
2026-05-26 14:24:56 +0530

    fix: force desktop layout on mobile via viewport width=1280

--- kode/resources/views/admin/layouts/master.blade.php
+++ kode/resources/views/admin/layouts/master.blade.php
@@ -3,7 +3,7 @@
 
   <head>
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=1280" />
     <meta name="csrf-token" content="{{csrf_token()}}" />
     <title>{{@site_settings("site_name")}} {{site_settings('title_separator')}} {{@translate($title)}}</title>
 
--- kode/resources/views/layouts/master.blade.php
+++ kode/resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
    >
   <head>
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=1280" />
     <meta name="csrf-token" content="{{csrf_token()}}" />
     <meta name="color-scheme" content="light dark" />
 
